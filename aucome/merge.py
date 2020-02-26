64080ebcbed0b745aef36d5da5df080b4e816849
Arnaud Belcour <arnaud.belcour@irisa.fr>
2020-02-26 10:15:28 +0100

    Fix issue in merge.

--- aucome/merge.py
+++ aucome/merge.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 usage:
-    aucome merge --run=ID [--cpu=INT] [-v]
+    aucome merge --run=ID [--cpu=INT] [-v] [-vv]
 
 options:
     --run=ID    Pathname to the comparison workspace.
