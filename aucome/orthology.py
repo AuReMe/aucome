d85d1d039a27724c53f280fc22e5886030a43ffd
Arnaud Belcour <arnaud.belcour@irisa.fr>
2020-02-26 14:31:36 +0100

    Add an error message when OrthoFinder results are missing.

--- aucome/orthology.py
+++ aucome/orthology.py
@@ -20,6 +20,7 @@ import docopt
 import os
 import subprocess
 import shutil
+import sys
 import time
 
 from padmet.classes import PadmetSpec
@@ -117,9 +118,19 @@ def run_orthology(run_id, orthogroups, sequence_search_prg, nb_cpu_to_use, filte
         if verbose:
             print("Orthofinder done in: %ss" %end_time)
         if orthogroups:
-            orthodata_path = max(["%s/%s" %(x[0], 'Orthogroups/Orthogroups.tsv') for x in os.walk(orthofinder_wd_path) if 'Orthogroups' in x[1]])
+            orthofinder_result_path = orthofinder_wd_path + '/OrthoFinder/'
+            orthogroups_result_path = orthofinder_result_path + os.listdir(orthofinder_result_path)[0] + '/Orthogroups'
+            if os.path.exists(orthogroups_result_path):
+                orthodata_path = max(["%s/%s" %(x[0], 'Orthogroups/Orthogroups.tsv') for x in os.walk(orthofinder_wd_path) if 'Orthogroups' in x[1]])
+            else:
+                sys.exit('There was an error with OrthoFinder, there is no results in ' + orthogroups_result_path)
         else:
-            orthodata_path = max(["%s/%s" %(x[0], 'Orthologues') for x in os.walk(orthofinder_wd_path) if 'Orthologues' in x[1]])
+            orthofinder_result_path = orthofinder_wd_path + '/OrthoFinder/'
+            orthologues_result_path = orthofinder_result_path + os.listdir(orthofinder_result_path)[0] + '/Orthologues'
+            if os.path.exists(orthologues_result_path):
+                orthodata_path = max(["%s/%s" %(x[0], 'Orthologues') for x in os.walk(orthofinder_wd_path) if 'Orthologues' in x[1]])
+            else:
+                sys.exit('There was an error with OrthoFinder, there is no results in ' + orthologues_result_path)
     if verbose:
         print("Parsing Orthofinder output %s" %orthodata_path)
 
