b9311a49e285207622f2852a3f233b6031a89cdd
Arnaud Belcour <arnaud.belcour@irisa.fr>
2020-02-26 10:18:34 +0100

    Add missing character in merge docopt argument.

--- aucome/merge.py
+++ aucome/merge.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 usage:
-    aucome merge --run=ID [--cpu=INT] [-v] [-vv]
+    aucome merge --run=ID [--cpu=INT] [-v] [--vv]
 
 options:
     --run=ID    Pathname to the comparison workspace.
