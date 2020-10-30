94cf21b2d097e272fd87bb72b152935f8043d404
Arnaud Belcour <arnaud.belcour@gmail.com>
2020-10-30 13:05:26 +0100

    Add __version__.

--- aucome/__init__.py
+++ aucome/__init__.py
@@ -1,1 +1,3 @@
 from aucome import analysis, check, compare, merge, reconstruction, orthology, utils, workflow, structural
+
+__version__='0.5'
--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 import os
 
 from io import open
-from setuptools import setup
+from distutils.util import convert_path
+from setuptools import setup, find_packages
 
 setup_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(setup_directory, 'README.rst'), encoding='utf-8') as readme_file:
     long_description = readme_file.read()
 
+init_data = {}
+init_pathname = convert_path('aucome/__init__.py')
+with open(init_pathname) as init_file:
+    exec(init_file.read(), init_data)
+
 setup(name='aucome',
       description='Automatic Comparison of Metabolism',
       long_description=long_description,
-      version='0.5',
+      version=init_data['__version__'],
       url='https://github.com/AuReMe/aucome',
       author='A. Belcour',
       author_email='arnaud.belcour@gmail.com',
@@ -30,7 +36,7 @@ setup(name='aucome',
       ],
       packages=['aucome'],
       install_requires=[
-            'mpwt==0.5.4',
+            'mpwt==0.5.5',
             'padmet==4.0',
             'eventlet==0.25.0',
             'requests==2.22.0',
